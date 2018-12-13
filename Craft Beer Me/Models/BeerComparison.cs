3b02059d703f5e0c3f374f4b9e7acd13ac29c11a
Sean Keckler <seankeckler@gmail.com>
2018-12-13 09:43:45 -0500

    added most of new search feature

---
+++ Craft Beer Me/Models/BeerComparison.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Craft_Beer_Me.Models
+{
+    public class BeerComparison
+    {
+        public int ABVScore { get; set; }
+        public int ABVCat { get; set; }
+        public int IBUScore { get; set; }
+        public int IBUCat { get; set; }
+        public int SRMScore { get; set; }
+        public int SRMCat { get; set; }
+
+        public BeerComparison()
+        {
+
+        }
+
+        public BeerComparison(int ABVScore, int ABVCat, int IBUScore, int IBUCat, int SRMScore, int SRMCat)
+        {
+            this.ABVScore = ABVScore;
+            this.ABVCat = ABVCat;
+            this.IBUScore = IBUScore;
+            this.IBUCat = IBUCat;
+            this.SRMScore = SRMScore;
+            this.SRMCat = SRMCat;
+        }
+
+    }
+}
